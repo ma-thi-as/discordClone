02af3e8c0dba6da40ec54f70b13fa108c19d0370
matdeb <mat@deb4u.deb4u>
2024-01-18 09:55:21 -0300

    Room config

--- index.js
+++ index.js
@@ -7,18 +7,36 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
+
+app.get('/home', (req, res) => {
+  res.sendFile(join(__dirname, 'templates/home.html'));
+
+})
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'public/index.html'));
 });
+
+// Ruta para las salas
+app.get('/room/:roomName', (req, res) => {
+  res.sendFile(__dirname + '/templates/room.html');
+});
+
+
+
+
+
+
+
 // Add a data structure to store connected clients and their corresponding socket objects
 const connectedClients = {};
-
+const roomData = {};
+// When a client get a connection with socket
 io.on('connection', (socket) => {
   var clientID = socket.id;
-  connectedClients[clientID] = socket; 
+  connectedClients[clientID] = socket;
 
+  // emit clientID for the all others users
   socket.broadcast.emit('client', { clientID });
-
   socket.on('offer', (offerData) => {
     // Send the offer to the specified user
     const targetSocket = connectedClients[offerData.user];
@@ -27,6 +45,7 @@ io.on('connection', (socket) => {
     }
   });
 
+  //when offer es generated response to the other user with the answer
   socket.on('answer', (answerData) => {
     // Send the answer to the specified user
     const targetSocket = connectedClients[answerData.user];
@@ -35,6 +54,7 @@ io.on('connection', (socket) => {
     }
   });
 
+  //handle ice candidates.
   socket.on('ice-candidate', (candidateData) => {
     // Send the ICE candidate to the specified user
     console.log(candidateData);
@@ -42,6 +62,12 @@ io.on('connection', (socket) => {
     if (targetSocket) {
       targetSocket.emit("ice-candidate", { candidate: candidateData.candidate, sender: clientID });
     }
+  });
+
+  socket.on('join-room', (roomName) => {
+    socket.join(roomName);
+    // Inform clients in the same room about the new user
+    io.to(roomName).emit('user-joined', { userID: clientID });
   });
 
   socket.on('disconnect', () => {
